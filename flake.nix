fada69234ec951b296d039cde8ec3e6f36bc6ac5
Reasel <reasel@pm.me>
2026-01-03 16:18:05 -0800

    Initial

---
+++ flake.nix
@@ -0,0 +1,58 @@
+# flake.nix
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    bakkesmod-nix = {
+      url = "github:AddG0/bakkesmod-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {self, nixpkgs, home-manager, bakkesmod-nix, ...}@inputs: {
+  # set up for NixOS
+  nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+    specialArgs = {inherit inputs;};
+    modules = [
+      ./configuration.nix
+      # Add Home Manager integration
+      home-manager.nixosModules.home-manager
+      {
+        nixpkgs.config.allowUnfree = true;  # Required for unfree packages
+
+        home-manager.useGlobalPkgs = true;
+        home-manager.useUserPackages = true;
+        home-manager.users.reasel = { pkgs, ... }: {  # Changed to "reasel" to match your user
+          imports = [ bakkesmod-nix.homeManagerModules.default ];
+
+          # Required: Set Home Manager stateVersion (match your NixOS version)
+          home.stateVersion = "25.11";
+
+
+          programs.bakkesmod = {
+            enable = true;
+            plugins = with pkgs.bakkesmod-plugins; [  # Customize plugins as needed
+              rocketstats
+              {
+                plugin = deja-vu-player-tracking;
+                extraConfig = ''
+                  cl_dejavu_enabled "1"
+                  cl_dejavu_ingame "1"
+                '';
+              }
+            ];
+            config = {  # Customize config options as needed (see repo docs for all options)
+              gui.scale = 1.0;
+              console.enabled = true;
+              ranked.showRanks = true;
+            };
+          };
+        };
+      }
+    ];
+  };
+};
+}
