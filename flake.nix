2db211bc4021be161b80a7b95ec26384824a3d14
Reasel <reasel@pm.me>
2026-01-03 17:03:24 -0800

    Turns out I need the hardware to switch on it.

--- flake.nix
+++ flake.nix
@@ -35,7 +35,7 @@
           programs.bakkesmod = {
             enable = true;
             plugins = with pkgs.bakkesmod-plugins; [  # Customize plugins as needed
-              rocketstats
+              IngameRank
               {
                 plugin = deja-vu-player-tracking;
                 extraConfig = ''
@@ -46,7 +46,7 @@
             ];
             config = {  # Customize config options as needed (see repo docs for all options)
               gui.scale = 1.0;
-              console.enabled = true;
+              console.enabled = false;
               ranked.showRanks = true;
             };
           };
